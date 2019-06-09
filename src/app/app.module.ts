54a4e5449737960f0975dbcde9db521f96d78e2b
radenverko <radenko.dct@gmail.com>
2019-06-09 08:37:22 -0600

    add font awesome and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,48 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavComponent } from "./nav/nav.component";
+import { FeedbackComponent } from "./feedback/feedback.component";
+import { NotesComponent } from "./notes/notes.component";
+import { NotfoundComponent } from "./notfound/notfound.component";
+import { Router, Routes, RouterModule } from "@angular/router";
+
+const appRoutes: Routes = [
+  {
+    path: "notes",
+    component: NotesComponent
+  },
+  {
+    path: "feedback",
+    component: FeedbackComponent
+  },
+  {
+    path: "",
+    component: NotesComponent,
+    pathMatch: "full"
+  },
+  {
+    path: "**",
+    component: NotfoundComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FeedbackComponent,
+    NotesComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
